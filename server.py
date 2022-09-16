107f71e8d168cb40f092af48b1ee961c1f6c79ff
Gustavo Basulto <gbasulto.guzman@gmail.com>
2022-09-16 19:28:46 -0300

    cambios

--- server.py
+++ server.py
@@ -13,15 +13,5 @@ def number(num1):
 def number2(num1, num2):
     return render_template('index.html', num1=num1, num2=num2)
 
-@app.route('/<int:num1>/<int:num2>/<string:col1>/<string:col2>')                           
-def numberycolor(num1, num2, col1, col2):
-    return render_template('index.html', num1=num1, num2=num2, col1=col1, col2=col2)
-
-
-
-@app.route('/play/<int:num>/<string:color>')                           
-def playc(num,color):
-    return render_template('index.html', num=num, color=color) 
-    
 if __name__=="__main__":
     app.run(debug=True)   
